b07f2cebbd2bae0474f21b851e3fb22112c10d5a
Mihail Tal <mihail.tal85@gmail.com>
2022-08-16 18:00:35 +0300

    greet Aliens added

---
+++ greetAliens.js
@@ -0,0 +1,9 @@
+
+function greetAliens(str){
+    for (const x of str ) {
+    console.log(`Oh powerful ${x}, we humans offer our unconditional surrender!`)
+}}
+
+const aliens = ["Blorgous", "Glamyx", "Wegord", "SpaceKing"];
+
+greetAliens(aliens);
