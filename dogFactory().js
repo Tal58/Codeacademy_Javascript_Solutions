002d4dd32dbd26504793323bf69e33d638fb2847
Mihail Tal <mihail.tal85@gmail.com>
2022-08-17 13:22:15 +0300

    dog factory updated

--- dogFactory().js
+++ dogFactory().js
@@ -6,3 +6,43 @@ function dogFactory(name, breed, weight){
     return newobj;
 }
 console.log(dogFactory('Joe', 'Pug', 27))
+
+const dogFactory = (name, breed, weight) => {
+    return {
+        _name: name,
+        _breed: breed,
+        _weight: weight,
+        get name() {
+            return this._name;
+        },
+        set name(newName) {
+            this._name = newName;
+        },
+        get breed() {
+            return this._breed;
+        },
+        set breed(newBreed) {
+            this._breed = newBreed;
+        },
+        get weight() {
+            return this._weight;
+        },
+        set weight(newWeight) {
+            this._weight = newWeight;
+        },
+        bark() {
+            return 'ruff! ruff!'
+        },
+        eatTooManyTreats(num) {
+          if (num > 0){
+            return console.log(this._weight +=1);}
+        },
+        print(){
+          console.log(`${this.name}, ${this.breed}, ${this.weight}, ${this.bark()}`);
+        }
+    }
+}
+
+let rex = dogFactory('Rex', 'Labrador', 25);
+rex.print();
+rex.eatTooManyTreats(1);
