2d2d67f5576b3f0f7d815998b8a3b93147772d14
Mihail Tal <mihail.tal85@gmail.com>
2022-08-17 11:22:52 +0300

    just cool stuff added

---
+++ justCoolStuff().js
@@ -0,0 +1,22 @@
+// function justCoolStuff(arr1,arr2){
+//     newwarr = [];
+//     for (const x of arr1){
+//         if (arr2.includes(x)){
+//             newwarr.push(x);
+//         }
+//     }
+//     return newwarr;
+    
+// }
+
+// ****with filter() method
+// function justCoolStuff(arr1,arr2){
+//    return arr1.filter(x => arr2.includes(x));
+    
+// }
+
+// const coolStuff = ['gameboys', 'skateboards', 'backwards hats', 'fruit-by-the-foot', 'pogs', 'my room', 'temporary tattoos'];
+
+// const myStuff = [ 'rules', 'fruit-by-the-foot', 'wedgies', 'sweaters', 'skateboards', 'family-night', 'my room', 'braces', 'the information superhighway']; 
+
+// console.log(justCoolStuff(myStuff, coolStuff))
